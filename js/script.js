98edb1f5881613b1087d0f0d69fa5095eb0c281d
Mexfut1978 <egarcia981@yahoo.com>
2020-07-16 20:53:33 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Sliding function section
+
+$(document).ready(function() {
+    $('#slides').superslides({
+        animation: 'fade', 
+        play: 5000, // speed of slider
+        pagination: false  // This is the bottom dots
+    });
+
+});
